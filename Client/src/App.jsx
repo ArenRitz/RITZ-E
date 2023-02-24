e5c516f319ed6b14ff42dcf5f4399df97aaca3c8
Aren Dolbakian <aren.dolbakian@gmail.com>
2023-02-24 16:35:55 -0500

    Completed initial UI for create page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,14 +4,15 @@ import { logo } from './assets';
 import { Home, CreatePost } from './pages';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
 
   return (
     <BrowserRouter>
       <header className='w-full flex justify-between items-center bg-wh
        sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
         <Link to="/">
-          <img src={logo} alt="logo" className='w-28 object-contain'/>
+          <img src={logo} alt="logo" className='w-28 object-contain' />
         </Link>
         <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
           Create
@@ -20,7 +21,7 @@ function App() {
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/create-post' element={<CreatePost/>} />
+          <Route path='/create-post' element={<CreatePost />} />
         </Routes>
       </main>
 
