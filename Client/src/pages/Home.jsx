1c9d8c39a75df7b1c40eab3495d45ec7db07eeda
Aren Dolbakian <aren.dolbakian@gmail.com>
2023-02-24 21:51:14 -0500

    Complete home page UI and add search function

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,49 +1,67 @@
 import { useState, useEffect } from 'react'
 import { Loader, Card, FormField } from '../Components'
 
-const RederCards = ({data, title}) => {
- if (data?.length > 0) {
-    return data.map((post) => <Card key={post._id} {...post} /> )
-}
+const RenderCards = ({ data, title }) => {
+    if (data?.length > 0) {
+        return (
+            data.map((post) => <Card key={post._id} {...post} />)
+        );
+    }
+
     return (
-        <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>
-    )
-
-}
-
+        <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
+    );
+};
 
 
 const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
     const [searchText, setSearchText] = useState('')
-
-
-    useEffect(() => {
-        const fetchPosts = async () => {
-            setLoading(true);
-            try {
-                const response = await fetch('http://localhost:8080/api/v1/post', {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                })
-
-                if(response.ok) {
-                    const result = await response.json();
-
-                    setAllPosts(result.data.reverse());
-                }
-            } catch (error) {
-                 alert(error)
-            } finally {
-                setLoading(false)
-            }
+    const [searchedResults, setSearchedResults] = useState(null);
+    const [searchTimeout, setSearchTimeout] = useState(null);
+    const fetchPosts = async () => {
+        setLoading(true);
+    
+        try {
+          const response = await fetch('http://localhost:8080/api/v1/post', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+    
+          if (response.ok) {
+            const result = await response.json();
+            setAllPosts(result.data.reverse());
+          }
+        } catch (err) {
+          alert(err);
+        } finally {
+          setLoading(false);
         }
-
+      };
+    
+      useEffect(() => {
         fetchPosts();
-    }, [])
+      }, []);
+    
+      const handleSearchChange = async (e) => {
+        
+        clearTimeout(searchTimeout);
+        setLoading(true)
+        setSearchText(e.target.value);
+        
+        setSearchTimeout(
+        setTimeout(() => {
+            setLoading(false)
+            const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+            setSearchedResults(searchResult);
+        }, 500)
+        
+    );
+    
+    }
 
 
 
@@ -54,35 +72,43 @@ const Home = () => {
                 <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'> Browse through a collection of imaginative and visually stunning images genearated by DALL-E AI </p>
             </div>
             <div className="mt-16">
-                <FormField/>
+                <FormField 
+                    labelName='Search posts'
+                    type="text"
+                    name="text"
+                    placeholder="Search posts"
+                    value={searchText}
+                    handleChange={handleSearchChange}
+                
+                />
             </div>
-            <div className='mt-10'> 
-            {loading ? (
-                <div className='flex justify-center items-center'>
-                    <Loader/>
-                </div>
-            ) :
-            ( 
-               <>
-                {searchText && (
-                    <h2 className='font-medium text-[#666e75] text-xl mb-3'> Showing results for <span className='text-[#222328]'>{searchText}</span> </h2>
-                )}
-                <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
-                    {searchText ? (
-                        <RederCards
-                            data={[]}
-                            title="No search results found"
-                            />
-                    ) : (
-                        <RederCards
-                        data={allPosts}
-                        title="No posts found"
-                        />
-                    )
-                    }
-                </div>
-               </> 
-            )}
+            <div className='mt-10'>
+                {loading ? (
+                    <div className='flex justify-center items-center'>
+                        <Loader />
+                    </div>
+                ) :
+                    (
+                        <>
+                            {searchText && (
+                                <h2 className='font-medium text-[#666e75] text-xl mb-3'> Showing results for <span className='text-[#222328]'>{searchText}</span> </h2>
+                            )}
+                            <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+                                {searchText ? (
+                                    <RenderCards
+                                        data={searchedResults}
+                                        title="No search results found"
+                                    />
+                                ) : (
+                                    <RenderCards
+                                        data={allPosts}
+                                        title="No posts found"
+                                    />
+                                )
+                                }
+                            </div>
+                        </>
+                    )}
             </div>
         </section>
     )
